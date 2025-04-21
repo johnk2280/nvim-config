be036ce07386678bd85ea5e5567a0b6e23f6e1eb
Evgeniy Kungurov <johnk2280@gmail.com>
2025-04-21 21:29:30 +0500

    [add] bufferline

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -18,3 +18,9 @@ vim.keymap.set('n', '\\', ':split<CR>')
 
 -- neo-tree
 vim.keymap.set('n', '<leader>e', ':Neotree left reveal toggle<CR>')
+
+-- bufferline
+vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+vim.keymap.set('n', '<c-x>', ':BufferLineCloseOthers<CR>')
+vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
