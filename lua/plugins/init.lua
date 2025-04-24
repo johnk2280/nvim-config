8b3106a45c7c606fd58649a07a13a5281d1c68f5
Evgeniy Kungurov <johnk2280@gmail.com>
2025-04-24 16:58:38 +0500

    [upd] plugins/init.lua - updated ensure_installed for treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', 
     opts = require "configs.conform",
   },
 
@@ -16,13 +16,41 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", 
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "csv",
+        "dockerfile",
+        "dot",
+        "fsharp",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "htmldjango",
+        "javascript",
+        "json",
+        "nginx",
+        "python",
+        "regex",
+        "requirements",
+        "rust",
+        "scss",
+        "sql",
+        "toml",
+        "tsv",
+        "tsx",
+        "typescript",
+        "vue",
+        "yaml",
+  		},
+  	},
+  },
 }
