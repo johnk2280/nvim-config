3e79b69b27e146ec5f56709e2f4635a144bf3ca0
Evgeniy Kungurov <johnk2280@gmail.com>
2025-05-13 11:31:22 +0300

    [upd] lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,7 +2,10 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = {
   "html",
+  "css_variables",
   "cssls",
+  "cssmodules_ls",
+  "docker_compose_language_service",
   "tsserver",
   "ruff",
   "pyright",
