e91d689d69d5b80a2c00c55e7a64c4db5f06839e
Evgeniy Kungurov <johnk2280@gmail.com>
2025-05-13 15:46:34 +0300

    [upd] mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Terminal
+map("t", "jj", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+map("t", "<ESC>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
