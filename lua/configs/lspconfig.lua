028f109762eb0cd270bb0c9786a71ab30fe636ec
Evgeniy Kungurov <johnk2280@gmail.com>
2025-05-13 14:23:49 +0300

    [upd] lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,18 +1,22 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = {
-  "html",
-  "css_variables",
-  "cssls",
-  "cssmodules_ls",
-  "dockerls",
-  "docker_compose_language_service",
-  "eslint_d",
-  "jsonls",
-  "nginx_language_server",
-  "ts_ls",
-  "ruff",
-  "rust_analyzer",
-  "pyright",
+  html = {},
+  css_variables = {},
+  cssls = {},
+  cssmodules_ls = {},
+  dockerls = {},
+  docker_compose_language_service = {},
+  eslint_d = {},
+  jsonls = {},
+  nginx_language_server = {},
+  ts_ls = {},
+  ruff = {},
+  rust_analyzer = {},
+  pyright = {},
 }
-vim.lsp.enable(servers)
+
+for name, opts in pairs(servers) do
+  vim.lsp.enable(name)
+  vim.lsp.config(name, opts)
+end
