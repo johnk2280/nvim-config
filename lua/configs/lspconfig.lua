07d369bf36d67e25acce060c0ab0e8bfb2a819a9
Evgeniy Kungurov <johnk2280@gmail.com>
2025-05-21 23:48:44 +0500

    [upd] lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,17 +1,21 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = {
+    dockerls = {},
+    docker_compose_language_service = {},
+
     html = require "configs.lsp.html",
     css_variables = require "configs.lsp.css_variables",
     cssls = require "configs.lsp.cssls",
     cssmodules_ls = require "configs.lsp.cssmodules_ls",
-    dockerls = {},
-    docker_compose_language_service = {},
     eslint_d = require "configs.lsp.eslint_d",
     jsonls = {},
-    nginx_language_server = {},
     ts_ls = require "configs.lsp.ts_ls",
+
+    nginx_language_server = {},
+
     rust_analyzer = {},
+
     isort = {},
     ruff = require "configs.lsp.ruff",
     pyright = require "configs.lsp.pyright",
